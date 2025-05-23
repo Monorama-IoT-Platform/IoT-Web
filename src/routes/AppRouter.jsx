9a106a89d4ba2254b6c20ad5df9aaba313bd9aba
Sangmin Han <gkstkdals07@naver.com>
2025-05-24 01:38:00 +0900

    :sparkles: Feat: 라우터 추가

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,59 @@
-import React from 'react';
+// import React from 'react';
+// import { Routes, Route, Navigate } from 'react-router-dom';
+// import LoginPage from '../pages/LoginPage';
+// import ProjectListPage from '../pages/ProjectListPage';
+// import { getAccessToken } from '../utils/tokenStorage'; 
+
+// const PrivateRoute = ({ children }) => {
+//   const token = getAccessToken();
+//   return token ? children : <Navigate to="/login" replace />;
+// };
+
+// const PublicRoute = ({ children }) => {
+//   const token = getAccessToken();
+//   return token ? <Navigate to="/projects" replace /> : children;
+// };
+
+// function AppRouter() {
+//   return (
+//     <Routes>
+//       <Route
+//         path="/login"
+//         element={
+//           <PublicRoute>
+//             <LoginPage />
+//           </PublicRoute>
+//         }
+//       />
+//       <Route
+//         path="/projects"
+//         element={
+//           <PrivateRoute>
+//             <ProjectListPage />
+//           </PrivateRoute>
+//         }
+//       />
+//       <Route path="*" element={<Navigate to="/login" replace />} />
+//     </Routes>
+//   );
+// }
+
+// export default AppRouter;
+
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage';
-// import ProjectListPage from '../pages/ProjectListPage';
-// import ProjectCreatePage from '../pages/ProjectCreatePage';
+import ProjectListPage from '../pages/ProjectListPage';
+import ProjectCreatePage from '../pages/ProjectCreatePage';
+import RegisterUserPage from '../pages/RegisterUserPage';
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
-      {/* <Route path="/projects" element={<ProjectListPage />} /> */}
-      {/* <Route path="/projects/create" element={<ProjectCreatePage />} /> */}
-      <Route path="*" element={<Navigate to="/login" replace />} />
+      <Route path="/auth/register/social" element={<RegisterUserPage />} />
+      <Route path="/projects" element={<ProjectListPage />} />
+      <Route path="/projects/create" element={<ProjectCreatePage />} />
+      {/* <Route path="*" element={<Navigate to="/login" replace />} /> */}
     </Routes>
   );
 }
